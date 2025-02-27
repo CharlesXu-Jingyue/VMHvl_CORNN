8ca3ca94c5e1ad55011322f65c00df45cd3f9d1a
Charles Xu <charlesxu@caltech.edu>
2025-02-27 11:00:56 -0800

    rnn sim

--- analysis/experiment4-line attractor/test_new_network.py
+++ analysis/experiment4-line attractor/test_new_network.py
@@ -6,7 +6,6 @@ Created on Wed May 10 11:48:03 2023
 @author: dinc
 """
 
-
 import numpy as np
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
@@ -22,160 +21,159 @@ from sklearn.metrics import r2_score
 
 # Define the network
 class Model(nn.Module):
-    def __init__(self, input_dims, hidden_dims,output_dims, alpha=0.1):
+    def __init__(self, input_dims, hidden_dims, output_dims, alpha=0.1):
         super(Model, self).__init__()
         self.input_dims = input_dims
         self.hidden_dims = hidden_dims
         self.output_dims = output_dims
         self.alpha = alpha
 
-        # initialize weights
-        self.W_in = nn.Parameter(torch.randn(hidden_dims,input_dims))
-        self.W_rec = nn.Parameter(torch.randn(hidden_dims, hidden_dims))
-        self.W_out = nn.Parameter(torch.randn(output_dims,hidden_dims))
+        # Initialize weights
+        self.W_in = nn.Parameter(torch.randn(hidden_dims, input_dims).to('cuda'))
+        self.W_rec = nn.Parameter(torch.randn(hidden_dims, hidden_dims).to('cuda'))
+        self.W_out = nn.Parameter(torch.randn(output_dims, hidden_dims).to('cuda'))
         nn.init.xavier_uniform_(self.W_in)
         nn.init.xavier_uniform_(self.W_rec)
         nn.init.xavier_uniform_(self.W_out)
 
-        # set W_rec diagonal to zero
-        self.W_rec.data = self.W_rec.data * (1 - torch.eye(hidden_dims))
+        # Set W_rec diagonal to zero
+        self.W_rec.data = self.W_rec.data * (1 - torch.eye(hidden_dims).to('cuda'))
 
-    
-    def forward(self, u, r,noise_in = 0.1, noise_con = 0):
-        
+    def forward(self, u, r, noise_in=0.1, noise_con=0):
         """
         Inputs
-        The shape of u is (seq_len,input_dims)
+        The shape of u is (seq_len, input_dims)
         The shape of r is (hidden_dims), initialization of hidden dims
         
         Outputs
         The shape of o is (seq_len, output)
-        The shape of hidden_states is (seq_len+1,hidden_dims)
+        The shape of hidden_states is (seq_len+1, hidden_dims)
         """
-        T = u.shape[0]
-        n_rec = r.shape[0]
+        T = u.shape[0]  # Sequence length
+        n_rec = r.shape[0]  # Number of hidden dimensions
         
-        hidden_states = torch.zeros([T+1,n_rec])
-        hidden_states[0,:] = r.flatten();
-        x = torch.arctanh(r)
-        o = torch.zeros([T,self.output_dims])
+        hidden_states = torch.zeros([T+1, n_rec]).to('cuda')
+        hidden_states[0, :] = r.flatten().to('cuda')
+        x = torch.arctanh(r).to('cuda')
+        o = torch.zeros([T, self.output_dims]).to('cuda')
         for t in range(T):
-            x = (1 - self.alpha) * x + self.alpha * (self.W_rec @ r + self.W_in @ u[t,:] \
-                + torch.normal(torch.zeros(n_rec),noise_in) )
-            r = torch.tanh(x) + torch.normal(torch.zeros(n_rec),noise_con) 
-            hidden_states[t+1,:] = r
-            o[t,:] = self.W_out @ r.flatten()
+            x = (1 - self.alpha) * x + self.alpha * (self.W_rec @ r + self.W_in @ u[t, :] \
+                + torch.normal(torch.zeros(n_rec), noise_in).to('cuda'))
+            r = torch.tanh(x).to('cuda') + torch.normal(torch.zeros(n_rec), noise_con).to('cuda')
+            hidden_states[t+1, :] = r
+            o[t, :] = self.W_out @ r.flatten()
                 
-        return hidden_states,o
+        return hidden_states, o
 
-num_trials  = 30;
-n_in        = 1;
-n_rec       = 5000;
-T           = 1000;
-width       = 20;
-gaus_int    = 500;
+num_trials = 30             # number of trials
+n_in = 1                    # input dimensions
+n_rec = 500                 # number of hidden dimensions
+T = 1000                    # sequence length
+width = 20                  # Gaussian width
+gaus_int = 500              # Gaussian intensity
 
-r_in_all =np.zeros([T*num_trials,n_rec])
-r_out_all =np.zeros([T*num_trials,n_rec])
-u_all =np.zeros([T*num_trials,n_in])
-    
-model = Model(n_in,n_rec,n_in)
-model = torch.load('model_0.pt')
+# Initialize arrays to store results
+r_in_all = np.zeros([T*num_trials, n_rec])
+r_out_all = np.zeros([T*num_trials, n_rec])
+u_all = np.zeros([T*num_trials, n_in])
+
+# Load the model
+model = Model(n_in, n_rec, n_in)
+# model = torch.load('models/model_0.pt', weights_only=False)
 
 for i in range(num_trials):
-    u      = torch.zeros([T,n_in])
-    u[0:100,0] = 1
-    out_gt = torch.tensor(np.exp(-((np.arange(T) - gaus_int)**2)/(2*width**2)))
+    u = torch.zeros([T, n_in]).to('cuda')
+    u[0:100, 0] = 1
+    out_gt = torch.tensor(np.exp(-((np.arange(T) - gaus_int)**2)/(2*width**2))).to('cuda')
     
-    r_init   = torch.rand(n_rec)-0.5
-    # forward
-    h,out = model(u,r_init)
+    r_init = torch.rand(n_rec).to('cuda') - 0.5
+
+    # Forward pass
+    h, out = model(u, r_init)
     
-    r = h.detach().numpy();
-    out = out.detach().numpy();
-    u = u.detach().numpy();
-    gnd = model.W_rec.data.detach().numpy().flatten()
-    u_all[i*T:(i+1)*T,:] = u;
-    r_in_all[i*T:(i+1)*T,:] = r[:-1,:];
-    r_out_all[i*T:(i+1)*T,:] = r[1:,:];
+    r = h.detach().cpu().numpy()
+    out = out.detach().cpu().numpy()
+    u = u.detach().cpu().numpy()
+    gnd = model.W_rec.data.detach().cpu().numpy().flatten()
+    u_all[i*T:(i+1)*T, :] = u
+    r_in_all[i*T:(i+1)*T, :] = r[:-1, :]
+    r_out_all[i*T:(i+1)*T, :] = r[1:, :]
         
     print(i)
 
-#%%
-n_sup = 500;
-nt=30
-r_in = r_in_all[:T*nt,:n_sup]
-r_out = r_out_all[:T*nt,:n_sup]
-u_in  = u_all[:T*nt,:]
+n_sup = 500 # number of supervised dimensions
+nt = 30 # number of trials for supervised learning
+r_in = r_in_all[:T*nt, :n_sup]
+r_out = r_out_all[:T*nt, :n_sup]
+u_in = u_all[:T*nt, :]
 
+# Ground truth weights
+gnd = model.W_rec.data.detach().cpu().numpy()
+gnd = gnd[:n_sup, :n_sup].flatten()
+alph = 0.1 + np.random.normal(0, 0, n_sup)
+w = solve_corrn_admm(r_in, r_out, u_in=u_in, alph=alph, l2=1e-5, 
+                     threshold=1, rho=1, verbose=2, num_iters=0,
+                     gnd=gnd, solver_type='weighted')
 
-gnd = model.W_rec.data.detach().numpy()
-gnd = gnd[:n_sup,:n_sup].flatten()
-alph = 0.1 + np.random.normal(0,0,n_sup)
-w = [];
-w = solve_corrn_admm(r_in,r_out,u_in = u_in, alph =alph , l2 = 1e-5, 
-                         threshold = 1, rho = 1,verbose = 2,num_iters = 0,
-                         gnd = gnd,solver_type = 'weighted')
+w_rec_cornn = w[:n_sup, :].T
+w_in_cornn = w[n_sup:, :].T
 
+w_rec_cornn[np.eye(n_sup, dtype=bool)] = 0
 
-
-w_rec_cornn = w[:n_sup,:].T
-w_in_cornn = w[n_sup:,:].T
-
-w_rec_cornn[np.eye(n_sup,dtype=(bool))] = 0
-
+# Plot and save the scatter plot for recurrent weights
 prd = w_rec_cornn.flatten()
-plt.scatter(gnd,prd)
-plt.show()
-print(pearsonr(gnd,prd))
+plt.scatter(gnd, prd)
+plt.title('Recurrent Weights Correlation')
+plt.xlabel('Ground Truth')
+plt.ylabel('Predicted')
+plt.savefig('figures/recurrent_weights_correlation.png')
+plt.close()
+print(pearsonr(gnd, prd))
 
-
-
-gnd_in = model.W_in.data.detach().numpy()[:n_sup];
+# Plot and save the scatter plot for input weights
+gnd_in = model.W_in.data.detach().cpu().numpy()[:n_sup]
 prd_in = w_in_cornn.flatten()
-plt.scatter(gnd_in,prd_in)
-plt.show()
-print(pearsonr(gnd_in.flatten(),prd_in))
+plt.scatter(gnd_in, prd_in)
+plt.title('Input Weights Correlation')
+plt.xlabel('Ground Truth')
+plt.ylabel('Predicted')
+plt.savefig('figures/input_weights_correlation.png')
+plt.close()
+print(pearsonr(gnd_in.flatten(), prd_in))
 
-
-
-
-opts = {};
+opts = {}
 opts['n_rec'] = n_sup
 opts['n_in'] = 3
 opts['alpha'] = alph
-opts['verbose'] = False;
+opts['verbose'] = False
 opts['sigma_input'] = 0
-opts['sigma_conversion'] =0 
+opts['sigma_conversion'] = 0 
 m1 = RNN(opts)
 m1.rnn['w_rec'] = w_rec_cornn
 m1.rnn['w_in'] = w_in_cornn
 
-u      = torch.zeros([T,n_in])
-u[0:100,0] = 1
-u[400:410,0] = 10
-out_gt = torch.tensor(np.exp(-((np.arange(T) - gaus_int)**2)/(2*width**2)))
+u = torch.zeros([T, n_in]).to('cuda')
+u[0:100, 0] = 1
+u[400:410, 0] = 10
+out_gt = torch.tensor(np.exp(-((np.arange(T) - gaus_int)**2)/(2*width**2))).to('cuda')
 
-r_init   = torch.rand(n_rec)-0.5
-# forward
-h,out = model(u,r_init,0)
+# Forward pass
+r_init = torch.rand(n_rec).to('cuda') - 0.5
+h, out = model(u, r_init, 0)
 
-r_gt = h.detach().numpy();
-out_gt = out.detach().numpy();
-u = u.detach().numpy();
-#r = m1.get_time_evolution(T = u.shape[0], u =u)
-r_cornn = [];
-r_cornn = m1.get_time_evolution(T = u.shape[0], u =u,r_in = r_gt[0,:n_sup])
+r_gt = h.detach().cpu().numpy()
+out_gt = out.detach().cpu().numpy()
+u = u.detach().cpu().numpy()
+r_cornn = m1.get_time_evolution(T=u.shape[0], u=u, r_in=r_gt[0, :n_sup])
 
+pick = 0
+plt.plot(r_cornn[:, pick], label='CORNN')
+plt.plot(r_gt[:, pick], label='Ground Truth')
+plt.title('Time Evolution of Neuron {}'.format(pick))
+plt.xlabel('Time')
+plt.ylabel('Activation')
+plt.legend()
+plt.savefig('figures/time_evolution_neuron_{}.png'.format(pick))
+plt.close()
 
-
-pick= 0;
-plt.plot(r_cornn[:,pick])
-plt.plot(r_gt[:,pick])
-plt.show()
-
-
-print(r2_score(r_gt[:,:n_sup].flatten(),r_cornn.flatten()))
-
-
-
+print(r2_score(r_gt[:, :n_sup].flatten(), r_cornn.flatten()))
--- analysis/experiment4-line attractor/torch_bptt_line_demo.py
+++ analysis/experiment4-line attractor/torch_bptt_line_demo.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jun 20 13:32:18 2022
-
 @author: dinc
+
+Modified: 20250213
+By Charles Xu @ Caltech
 """
 
 import numpy as np
@@ -14,10 +16,11 @@ import torch.nn as nn
 import torch.optim as optim
 import random
 import time as time_now
+import os
 
 # Define the network
 class Model(nn.Module):
-    def __init__(self, input_dims, hidden_dims,output_dims, alpha=0.1):
+    def __init__(self, input_dims, hidden_dims, output_dims, alpha=0.1):
         super(Model, self).__init__()
         self.input_dims = input_dims
         self.hidden_dims = hidden_dims
@@ -25,9 +28,9 @@ class Model(nn.Module):
         self.alpha = torch.tensor(alpha).to('cuda')
 
         # initialize weights
-        self.W_in = nn.Parameter(torch.randn(hidden_dims,input_dims))
+        self.W_in = nn.Parameter(torch.randn(hidden_dims, input_dims))
         self.W_rec = nn.Parameter(torch.randn(hidden_dims, hidden_dims))
-        self.W_out = nn.Parameter(torch.randn(output_dims,hidden_dims))
+        self.W_out = nn.Parameter(torch.randn(output_dims, hidden_dims))
         nn.init.xavier_uniform_(self.W_in)
         nn.init.xavier_uniform_(self.W_rec)
         nn.init.xavier_uniform_(self.W_out)
@@ -36,76 +39,76 @@ class Model(nn.Module):
         self.W_rec.data = self.W_rec.data * (1 - torch.eye(hidden_dims))
         self.to('cuda')
 
-    
-    def forward(self, u, r,noise = 0.1):
-        
+    def forward(self, u, r, noise=0.1):
         """
         Inputs
-        The shape of u is (seq_len,input_dims)
+        The shape of u is (seq_len, input_dims)
         The shape of r is (hidden_dims), initialization of hidden dims
         
         Outputs
         The shape of o is (seq_len, output)
-        The shape of hidden_states is (seq_len+1,hidden_dims)
+        The shape of hidden_states is (seq_len+1, hidden_dims)
         """
-        T = u.shape[0]
-        n_rec = r.shape[0]
+        T = u.shape[0]  # sequence length
+        n_rec = r.shape[0]  # number of hidden dimensions
         
-        hidden_states = torch.zeros([T+1,n_rec]).to('cuda')
-        hidden_states[0,:] = r.flatten().to('cuda');
+        hidden_states = torch.zeros([T+1, n_rec]).to('cuda')
+        hidden_states[0, :] = r.flatten().to('cuda')
         x = torch.arctanh(r).to('cuda')
-        o = torch.zeros([T,self.output_dims]).to('cuda')
+        o = torch.zeros([T, self.output_dims]).to('cuda')
         for t in range(T):
-            x = (1 - self.alpha) * x + self.alpha * (self.W_rec @ r + self.W_in @ u[t,:] \
-                + torch.normal(torch.zeros(n_rec),noise).to('cuda') )
+            x = (1 - self.alpha) * x + self.alpha * (self.W_rec @ r + self.W_in @ u[t, :] \
+                + torch.normal(torch.zeros(n_rec), noise).to('cuda'))
             r = torch.tanh(x)
-            hidden_states[t+1,:] = r
-            o[t,:] = self.W_out @ r.flatten()
+            hidden_states[t+1, :] = r
+            o[t, :] = self.W_out @ r.flatten()
                 
-        return hidden_states,o
+        return hidden_states, o
 
-n_in        = 1;
-n_rec       = 5000;
-n_reg       = 200;
-num_trials  = 3000;
-T           = 600;
-width       = 20;
-gaus_int    = T-100;
-num_exps    = 10;
+# Define hyperparameters
+n_in = 1                # input dimensions
+n_rec = 500             # hidden dimensions
+n_reg = 200             # not used in the code
+num_trials = 1500       # number of training trials
+T = 600                 # sequence length
+width = 20              # width of the Gaussian
+gaus_int = T - 100      # center of the Gaussian
+num_exps = 1            # number of experiments
+
+if not os.path.exists('figures'):
+    os.makedirs('figures')
+if not os.path.exists('models'):
+    os.makedirs('models')
 
 for k in range(num_exps):
     # instantiate the model
-    model = Model(n_in,n_rec,n_in)
+    model = Model(n_in, n_rec, n_in)
     
-    
-    # define the multi target logistic regression loss function
+    # define the mean squared error loss function
     criterion = nn.MSELoss()
     
-    
-    #optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-7)
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9,weight_decay=1e-7)
-    
+    # define the optimizer
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-7)
     
     # train the model
     for epoch in range(num_trials):
-        u      = torch.zeros([T,n_in]).to('cuda')
-        out_gt = torch.zeros([T,n_in]).to('cuda')
-        r_in   = torch.rand(n_rec).to('cuda')-0.5
+        u = torch.zeros([T, n_in]).to('cuda')  # input sequence
+        out_gt = torch.zeros([T, n_in]).to('cuda')  # ground truth output
+        r_in = torch.rand(n_rec).to('cuda') - 0.5  # initial hidden state
     
-        u[0:100,0] = 1
-        out_gt[:,0] = torch.tensor(np.exp(-((np.arange(T) - gaus_int)**2)/(2*width**2))).to('cuda')
+        u[0:100, 0] = 1  # set the first 100 time steps of the input to 1
+        out_gt[:, 0] = torch.tensor(np.exp(-((np.arange(T) - gaus_int)**2) / (2 * width**2))).to('cuda')
         
-        # forward
-        h,out = model(u,r_in)
+        # forward pass
+        h, out = model(u, r_in) # hidden states and output
     
         # compute the loss
-        loss = criterion(out, out_gt) 
+        loss = criterion(out, out_gt)
         
-        # backward
+        # backward pass
         loss.backward()
         
         # update the weights
-        #torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
         optimizer.step()
         
         # Ensure the diagonal is zero 
@@ -114,54 +117,55 @@ for k in range(num_exps):
         # zero the gradients
         optimizer.zero_grad()
         
-        
-        # print the loss 
+        # print the loss and plot the results every 300 epochs
         if epoch % 300 == 0:
+            plt.figure(figsize=(10, 15))
             temp = time_now.localtime()
             current_time = time_now.strftime("%H:%M:%S", temp)
-            print('{}: epoch {}: loss {}' .format(current_time, epoch, criterion(out, out_gt).item()))
-            r_in   = torch.rand(n_rec).to('cuda')-0.5
-            u      = torch.zeros([T,n_in]).to('cuda')
-            u[0:100,0] = 1
-            h_no,out = model(u,r_in)
-            plt.subplot(5,1,4)
-            plt.plot(out.cpu().detach().numpy()[:,0])
-            plt.plot(out_gt.cpu().detach().numpy()[:,0])
-            plt.subplot(5,1,5)
-            u[300:350,0] = .5
-            h_yes,out = model(u,r_in)
-            plt.plot(out.cpu().detach().numpy()[:,0])
-            plt.plot(out_gt.cpu().detach().numpy()[:,0])
+            print('{}: epoch {}: loss {}'.format(current_time, epoch, criterion(out, out_gt).item()))
+
+            r_in = torch.rand(n_rec).to('cuda') - 0.5
+            u = torch.zeros([T, n_in]).to('cuda')
+
+            u[0:100, 0] = 1
+            h_no, out = model(u, r_in) # hidden states and output without additional input
+                        
+            plt.subplot(5, 1, 4)
+            plt.plot(out.cpu().detach().numpy()[:, 0])
+            plt.plot(out_gt.cpu().detach().numpy()[:, 0])
+            plt.title('Output without additional input')
+
+            u[300:350, 0] = 0.5
+            h_yes, out = model(u, r_in) # hidden states and output with additional input
             
+            plt.subplot(5, 1, 5)
+            plt.plot(out.cpu().detach().numpy()[:, 0])
+            plt.plot(out_gt.cpu().detach().numpy()[:, 0])
+            plt.title('Output with additional input')
             
-            h_no = h_no.cpu().detach().numpy()
-            h_yes = h_yes.cpu().detach().numpy()
+            h_no = h_no.cpu().detach().numpy() # hidden states without additional input
+            h_yes = h_yes.cpu().detach().numpy() # hidden states with additional input
             
-            plt.subplot(5,1,1)
-            plt.plot(h_no[:,0])
-            plt.plot(h_yes[:,0])
+            plt.subplot(5, 1, 1)
+            plt.plot(h_no[:, 0])
+            plt.plot(h_yes[:, 0])
+            plt.title('Hidden state 0')
+
+            plt.subplot(5, 1, 2)
+            plt.plot(h_no[:, 2])
+            plt.plot(h_yes[:, 2])
+            plt.title('Hidden state 2')
             
-            plt.subplot(5,1,2)
-            plt.plot(h_no[:,2])
-            plt.plot(h_yes[:,2])
+            plt.subplot(5, 1, 3)
+            plt.plot(h_no[:, 4])
+            plt.plot(h_yes[:, 4])
+            plt.title('Hidden state 4')
             
-            plt.subplot(5,1,3)
-            plt.plot(h_no[:,4])
-            plt.plot(h_yes[:,4])
-            
-            plt.show()
-            
-        
+            plt.tight_layout()
+            plt.savefig(f'figures/epoch_{epoch}.png')
+            plt.close()
+
+            print(h_no.shape)
+        print('\rEpoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_trials, loss.item()), end='')
     
-    torch.save(model, 'model_%d.pt' %k)
-
-
-
-
-
-
-
-
-
-
-
+    torch.save(model, 'models/model_%d.pt' % k)
